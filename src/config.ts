1c2490bbf59e2d96cedbea517514d277c542ccd1
Gonzalo <errandoneagonzalo18@gmail.com>
2024-08-11 20:05:57 -0300

    add APIs

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,12 @@
 const token = import.meta.env.VITE_TOKEN || '';
 const API_START = import.meta.env.VITE_API;
-const API = API_START + `products?token=${token}`;
+
+const API_GET_PRODUCTS = API_START + `products?token=${token}`;
+const API_GET_USER = API_START + 'user/me'
+const API_ADD_POINTS = API_START + 'user/points'
+const API_GET_HISTORY = API_START + 'user/history'
+const API_REEDEM = API_START + '/redeem'
+
 const productsPerPage = 10
 
-export {API, productsPerPage}
+export {API_GET_PRODUCTS, API_GET_USER, API_ADD_POINTS, API_GET_HISTORY, API_REEDEM, productsPerPage}
